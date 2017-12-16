d1ca9f462069c9db5ae46781d45327cb6f9bb95f
Sri Harsha Chilakapati <hello@goharsha.com>
2017-12-17 01:14:52 +0530

    Allow the app to be installed globally

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+#!/usr/bin/env node
 const notifier = require('node-notifier');
+
+notifier.notify({
+    title: "Remind me Health!",
+    message: "Go get some water now!"
+});
 
 setInterval(() => {
     notifier.notify({
